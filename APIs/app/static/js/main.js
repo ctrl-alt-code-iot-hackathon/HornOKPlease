e34103f667f491f6b74ae94bb7e7a3c61fae318f
shreayan98c <shreayan98c@gmail.com>
2019-03-12 02:59:02 +0530

    Made the controller page functional

--- APIs/app/static/js/main.js
+++ APIs/app/static/js/main.js
@@ -1,1 +1,65 @@
 console.log("LOADED!!!")
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+  console.log("Left button clicked!");
+  $.ajax({
+    url: "/left",
+    method: "get",
+    success: function(){
+      console.log("sent request!")
+    },
+    error: function(){
+      console.log("failed!")
+    }
+    })
+});
+});
+
+$(document).ready(function(){
+  $("#btn2").click(function(){
+  console.log("Up button clicked!");
+  $.ajax({
+    url: "/up",
+    method: "get",
+    success: function(){
+      console.log("sent request!")
+    },
+    error: function(){
+      console.log("failed!")
+    }
+    })
+});
+});
+
+$(document).ready(function(){
+  $("#btn3").click(function(){
+  console.log("Right button clicked!");
+  $.ajax({
+    url: "/right",
+    method: "get",
+    success: function(){
+      console.log("sent request!")
+    },
+    error: function(){
+      console.log("failed!")
+    }
+    })
+});
+});
+
+$(document).ready(function(){
+  $("#btn4").click(function(){
+  console.log("Down button clicked!");
+  $.ajax({
+    url: "/down",
+    method: "get",
+    success: function(){
+      console.log("sent request!")
+    },
+    error: function(){
+      console.log("failed!")
+    }
+    })
+});
+});
