e51edb8e649628bc1ee0357ea6fe97a1a0016fb1
Siddharth Jain <sidjain1412@gmail.com>
2019-03-11 18:51:44 +0530

    Added function to insert

--- APIs/app/views.py
+++ APIs/app/views.py
@@ -1,21 +1,27 @@
 from app import db
-from app.models import User, Message
+from app.models import Driver, Vehicles
 
 
-def verify_username(username):
-    user = User.query.filter_by(username=username).first()
-    if not user:
-        new_user = User(username=username)
-        db.session.add(new_user)
-        db.session.commit()
-
-
-def get_all_messages():
-    return Message.query.all()
-
-
-def add_message(message, username):
-    sender_id = User.query.filter_by(username=username).first().id
-    new_msg = Message(message=message, sender=sender_id)
-    db.session.add(new_msg)
+def insert_details(username, license, gender, dob, contact):
+    new_driver = Driver(username = username, license = license, gender = gender, dob = dob, contact = contact)
+    db.session.add(new_driver)
     db.session.commit()
+
+
+# def verify_username(username):
+#     user = User.query.filter_by(username=username).first()
+#     if not user:
+#         new_user = User(username=username)
+#         db.session.add(new_user)
+#         db.session.commit()
+
+
+# def get_all_messages():
+#     return Message.query.all()
+
+
+# def add_message(message, username):
+#     sender_id = User.query.filter_by(username=username).first().id
+#     new_msg = Message(message=message, sender=sender_id)
+#     db.session.add(new_msg)
+#     db.session.commit()
