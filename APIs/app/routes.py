15c90d1d59a69b5a697ef77c095303e398d5b095
Siddharth Jain <sidjain1412@gmail.com>
2019-03-11 18:51:21 +0530

    Fixing route

--- APIs/app/routes.py
+++ APIs/app/routes.py
@@ -1,33 +1,34 @@
 from flask import render_template, request, redirect, url_for
-from app.views import verify_username, get_all_messages, add_message
+from app.views import insert_details
 from app import app
 from flask import jsonify
 
 
 @app.route('/')
 def index(error=None):
-    return render_template('login.html', error=error)
+    return render_template('index.html', error=error)
 
 
 @app.route('/insert', methods=["POST"])
 def take_details():
-    try:
-        username = request.form['name']
-        license = request.form['license']
-        gender = request.form['gender']
-        dob = request.form['date']
-        contact = request.form['phone']
-        
-        if username:
-            verify_username(username)
-            messages = get_all_messages()
-            return render_template('chat.html', username=username,
-                                   messages=messages)
-        else:
+    if request.method == "POST":
+        try:
+            username = request.form['name']
+            license = request.form['Licence']
+            gender = request.form['gender']
+            dob = request.form['date']
+            contact = request.form['phone']
+
+            try:
+                insert_details(username, license, gender, dob, contact)
+                return render_template('main.html', username=username)
+            except Exception as e:
+                return render_template('index.html', error = e)
+        except Exception as e:
+            print(str(e))
             return redirect(url_for('index'))
-    except Exception as e:
-        print(str(e))
-        return redirect(url_for('index'))
+    else:
+        print("Wrong")
 
 
 @app.route('/test', methods=["GET"])
